bf15d7a3a6e854835cc60b6e944f301d888456cc
LF56 <leah.fox7@gmail.com>
2022-06-01 21:21:17 -0400

    added styling to the review page

--- client/src/pages/SingleBook.js
+++ client/src/pages/SingleBook.js
@@ -15,40 +15,54 @@ function SingleBook() {
 
   return (
     <>
-    <Link to="/">←Back To Search</Link>
-
-    <div className="uk-child-width-1-3@s uk-padding top" uk-grid>
-      <div>
-        <div className="uk-panel uk-margin-top  uk-object-scale-down">
-          <h2>{title}</h2>
-          <h4>{authors}</h4>
-          <img src={image} alt={`cover of ${title}`} />
-          <p>{description}</p></div>
-      </div>
-      <div >
-        <h2>Reviews</h2>
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          reviews.map((review, i) => (
-            <div key={`rev_${i}`}>
-              <p>
-                {review.username} at {review.createdAt}
-              </p>
-              <p>{review.reviewText}</p>
-            </div>
-          ))
-        )}
-      </div>
-      <form className="comment-form">
-        <div className="uk-panel uk-margin-top  uk-object-scale-down">
-            <textarea name="comment-body" id="comment-body" className="uk-textarea uk-border-rounded" rows="10" cols="100"
-                placeholder="Add Comment Here"></textarea>
-            <button className="uk-button uk-button-danger uk-position-default uk-border-rounded" type="submit">ADD YOUR REVIEW</button>
+      <Link to="/">←Back To Search</Link>
+      <div className="uk-child-width-1-3@m uk-grid-small uk-grid-match"
+        uk-grid="true">
+        <div>
+          <div class="uk-card uk-card-default" id="single-book">
+            <div class="uk-card-media-top">
+              <h2>{title}</h2>
+              <h4>{authors}</h4>
+              <img src={image} alt={`cover of ${title}`} />
+              <p>{description}</p></div>
+          </div>
         </div>
-        </form>
 
-    </div>
+
+        <div className="uk-child-width-1-3@m uk-grid-small uk-grid-match"
+          uk-grid="true">
+          <div>
+            <div class="uk-card uk-card" id="rewiews">
+              <h2>My Reviews</h2>
+              {loading ? (
+                <div>Loading...</div>
+              ) : (
+                reviews.map((review, i) => (
+                  <div key={`rev_${i}`}>
+                    <p>
+                      {review.username} at {review.createdAt}
+                    </p>
+                    <p>{review.reviewText}</p>
+                  </div>
+                ))
+              )}
+            </div>
+            <form className="comment-form">
+              <div className="uk-panel uk-margin-top  uk-object-scale-down">
+                <textarea name="comment-body" id="comment-body" className="uk-textarea uk-border-rounded" rows="10" cols="100"
+                  placeholder="ADD REVIEW HERE"></textarea>
+                <button className="uk-button uk-button-danger uk-position-default uk-border-rounded" type="submit">ADD YOUR REVIEW</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
     </>
 
   );
