c9578e0675210f8162723f4ec134c1e3b5137fb2
Logan Palmer <loganpalmer95@icloud.com>
2022-06-02 17:29:57 -0400

    push before pull

---
+++ client/src/pages/HaveRead.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HaveRead = () => {
+
+    return (
+        <>
+        <div>
+           <h1>Hello World</h1>
+        </div>
+        </>
+    );
+};
+export default HaveRead
