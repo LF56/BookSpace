8dc69da207256e182a28489e2cdd85a671c9ba95
LF56 <leah.fox7@gmail.com>
2022-06-01 20:53:10 -0400

    added styling to single book page, with a link back to search

--- client/src/pages/SingleBook.js
+++ client/src/pages/SingleBook.js
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 import { useQuery, useMutation } from "@apollo/client";
 import { CREATE_REVIEW } from "../utils/mutations";
 import { QUERY_REVIEWS } from "../utils/queries";
+import { Link } from "react-router-dom";
 
 function SingleBook() {
   const location = useLocation();
@@ -13,25 +14,43 @@ function SingleBook() {
   const reviews = data?.reviews || [];
 
   return (
-    <div>
-      <h2>{title}</h2>
-      <h4>{authors}</h4>
-      <img src={image} alt={`cover of ${title}`} />
-      <p>{description}</p>
-      <h2>Reviews</h2>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        reviews.map((review, i) => (
-          <div key={`rev_${i}`}>
-            <p>
-              {review.username} at {review.createdAt}
-            </p>
-            <p>{review.reviewText}</p>
-          </div>
-        ))
-      )}
+    <>
+    <Link to="/">←Back To Search</Link>
+
+    <div className="uk-child-width-1-3@s uk-padding top" uk-grid>
+      <div>
+        <div className="uk-panel uk-margin-top  uk-object-scale-down">
+          <h2>{title}</h2>
+          <h4>{authors}</h4>
+          <img src={image} alt={`cover of ${title}`} />
+          <p>{description}</p></div>
+      </div>
+      <div >
+        <h2>Reviews</h2>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          reviews.map((review, i) => (
+            <div key={`rev_${i}`}>
+              <p>
+                {review.username} at {review.createdAt}
+              </p>
+              <p>{review.reviewText}</p>
+            </div>
+          ))
+        )}
+      </div>
+      <form className="comment-form">
+        <div className="uk-panel uk-margin-top  uk-object-scale-down">
+            <textarea name="comment-body" id="comment-body" className="uk-textarea uk-border-rounded" rows="10" cols="100"
+                placeholder="Add Comment Here"></textarea>
+            <button className="uk-button uk-button-danger uk-position-default uk-border-rounded" type="submit">ADD YOUR REVIEW</button>
+        </div>
+        </form>
+
     </div>
+    </>
+
   );
 }
 
